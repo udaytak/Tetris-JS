b896d06df3922358c4aa6452968ea3941084cec8
Uday Tak <tak.uday@gmail.com>
2020-05-25 12:33:29 -0700

    basic JS reading individual grid and overall border

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const width = 10
+
+    console.log(squares)
 
 })
