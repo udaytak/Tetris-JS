fe9c4d9d3f2fd3b323e480ba704b1130d8274530
Uday Tak <tak.uday@gmail.com>
2020-05-25 14:18:57 -0700

    Tetrominos - l z t o i

--- app.js
+++ app.js
@@ -13,10 +13,38 @@ document.addEventListener('DOMContentLoaded', () => {
         [0, width,width+1,width+2]
     ]
 
-    const theTetrominos = [lTetromino]
+    const zTetromino = [
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1],
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1]
+      ]
+    
+      const tTetromino = [
+        [1,width,width+1,width+2],
+        [1,width+1,width+2,width*2+1],
+        [width,width+1,width+2,width*2+1],
+        [1,width,width+1,width*2+1]
+      ]
+    
+      const oTetromino = [
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1]
+      ]
+    
+      const iTetromino = [
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3]
+      ]
+
+      const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
     let currentPosition = 4
-    let current = lTetromino[3]/*theTetrominos[0][0]*/
+    let current = theTetrominos[0][0]
 
 
     //draw tetromino
