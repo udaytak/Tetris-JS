e8dd70bf2c596a9b36a5b400e283530b1e6428ac
Uday Tak <tak.uday@gmail.com>
2020-05-25 14:11:44 -0700

    lTetromino draw function

--- app.js
+++ app.js
@@ -5,6 +5,31 @@ document.addEventListener('DOMContentLoaded', () => {
     const StartBtn = document.querySelector('#start-button')
     const width = 10
 
-    console.log(squares)
+    //Tetrominos - l z t o i
+    const lTetromino = [
+        [1,1+width, 1+width*2, 2],
+        [width,width+1,width+2,width*2+2],
+        [1,1+width, 1+width*2, width*2],
+        [0, width,width+1,width+2]
+    ]
+
+    const theTetrominos = [lTetromino]
+
+    let currentPosition = 4
+    let current = lTetromino[3]/*theTetrominos[0][0]*/
+
+
+    //draw tetromino
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('tetromino')
+            console.log(squares[currentPosition + index].classList)
+        })
+    }
+
+    draw()
+    
+
+    
 
 })
