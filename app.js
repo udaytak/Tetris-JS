28093383cb475e17c242ec8ff6ddc1754aa031d7
Uday Tak <tak.uday@gmail.com>
2020-05-25 12:38:36 -0700

    js creating vcariable for all te ids and classes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-button')
     const width = 10
 
     console.log(squares)
