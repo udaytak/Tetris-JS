dbabeca42cbab6672c7d7705f902c841c6c1b001
Uday Tak <tak.uday@gmail.com>
2020-05-27 22:18:02 -0700

    move right function

--- app.js
+++ app.js
@@ -72,6 +72,15 @@ document.addEventListener('DOMContentLoaded', () => {
         if(e.keyCode === 37){
             moveLeft()
         }
+        else if (e.keyCode === 38){
+            //rotate()
+        }
+        else if (e.keyCode === 39){
+            moveRight()
+        }
+        else if (e.keyCode === 40){
+            moveDown()
+        }
     }
 
     document.addEventListener('keyup', control)
@@ -112,7 +121,19 @@ document.addEventListener('DOMContentLoaded', () => {
         draw()
     }
     
-    
+    //move the tetromino left, unless is at the edge or there is a blockage
+    function moveRight(){
+        undraw()
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === width-1) 
+
+        if(!isAtLeftEdge) currentPosition += 1
+        
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+            currentPosition -=  1
+        }
+
+        draw()
+    }  
 
     
 
