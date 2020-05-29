b98a4aaf2138401d85b2a3fe1f0848c932137b37
Uday Tak <tak.uday@gmail.com>
2020-05-28 22:10:32 -0700

    remove row and taken class

--- app.js
+++ app.js
@@ -199,11 +199,11 @@ document.addEventListener('DOMContentLoaded', () => {
             const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
 
             if(row.every(index => squares[index].classList.contains('taken'))){
-                score += 10
+                score += 1
                 scoreDisplay.innerHTML = score
                 row.forEach(index => {
                     squares[index].classList.remove('taken')
-                    squares[index].classList.remove('tetromino')
+                    //squares[index].classList.remove('tetromino')
                 })
                 const squaresRemoved = squares.splice(i,width)
                 squares = squaresRemoved.concat(squares)
